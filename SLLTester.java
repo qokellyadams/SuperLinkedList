e31bab1660cf2c23276928b968832aadd2ff43d7
Mrs. Adams <colleen_m_adams@mcpsmd.org>
2024-11-09 04:02:42 +0000

    classes

---
+++ SLLTester.java
@@ -0,0 +1,5 @@
+class SLLTester {
+  public static void main(String[] args) {
+
+  } // end main
+} // end class
---
+++ SuperLinkedList.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class SuperLinkedList extends LinkedList<String> {
+    public boolean removeVowels() {
+        return false;
+    }
+
+    public boolean removeConsonants() {
+        return false;
+    }
+
+    public LinkedList<String> removeDuplicates() {
+        return null;
+    }
+
+    public LinkedList<String> concatenateStrings() {
+
+        return null;
+    }
+
+    public LinkedList<String> mix(LinkedList<String> list2) {
+
+        return null;
+    }
+
+    public String toString() {
+
+        return "";
+    }
+} // end SuperLinkedList
